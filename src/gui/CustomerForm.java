083e95cf6650c22c3ae0b9b0150c9b379e6f8cc0
20111375 <20111375@student.accross.ac.uk>
2013-11-05 17:45:22 +0000

    Gui forms

---
+++ src/gui/CustomerForm.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 20111375
+ * Date: 05/11/2013
+ * Time: 16:16
+ */
+public class CustomerForm {
+    private JPanel MainWindow;
+    private JList list1;
+    private JTextField textField1;
+    private JTextField textField2;
+}
