a338f70cd1d183585c9738f55e35aed25cdab6bf
20111375 <20111375@student.accross.ac.uk>
2013-11-06 16:18:47 +0000

    Setup office pc and begun working on the forms

--- src/gui/CustomerForm.java
+++ src/gui/CustomerForm.java
@@ -11,6 +11,12 @@ import javax.swing.*;
 public class CustomerForm {
     private JPanel MainWindow;
     private JList list1;
+    private JTextField FirstNameField;
+    private JTextField SurnameField;
     private JTextField textField1;
     private JTextField textField2;
+    private JTextField textField3;
+    private JButton submitButton;
+    private JList list2;
+    private JButton button1;
 }
