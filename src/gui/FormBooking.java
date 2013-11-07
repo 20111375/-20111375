e11291a1c7f22cd951ff340a4c3dd97d18ffd9d3
20111375 <20111375@student.accross.ac.uk>
2013-11-07 14:24:11 +0000

    UI changes (bug found in jGoodies).

--- src/gui/FormBooking.java
+++ src/gui/FormBooking.java
@@ -5,11 +5,13 @@ import javax.swing.*;
 /**
  * Created by Andrew on 07/11/13.
  */
-public class test {
+public class FormBooking {
     private JComboBox comboBox1;
     private JList list1;
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JTextPane textPane1;
+    private JTextPane textPane2;
+    private JButton button4;
 }
---
+++ src/gui/ReportBookingSummary.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Andrew on 07/11/13.
+ */
+public class ReportBookingSummary {
+    private JPanel panel1;
+}
