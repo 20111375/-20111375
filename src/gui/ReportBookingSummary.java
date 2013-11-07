7b39718bb103345e60a230a301e591ecb5787785
20111375 <20111375@student.accross.ac.uk>
2013-11-07 15:51:24 +0000

    UI changes (bug found in jGoodies).

--- src/gui/ReportBookingSummary.java
+++ src/gui/ReportBookingSummary.java
@@ -7,4 +7,11 @@ import javax.swing.*;
  */
 public class ReportBookingSummary {
     private JPanel panel1;
+    private JTextPane textPane1;
+    private JTextPane textPane2;
+    private JTextPane textPane3;
+    private JTextPane textPane4;
+    private JTextPane textPane5;
+    private JTextPane textPane6;
+    private JTextPane textPane7;
 }
