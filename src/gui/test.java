afaf77ef3735ee3f78ce076f1a25997a7f4cfbbb
20111375 <20111375@student.accross.ac.uk>
2013-11-07 14:09:23 +0000

    UI changes (bug found in jGoodies).

---
+++ src/gui/test.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Andrew on 07/11/13.
+ */
+public class test {
+    private JComboBox comboBox1;
+    private JList list1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTextPane textPane1;
+}
